6fc3fa1f966ac71efd485ab63dbf88836a1193e9
JoceDav <di22143@dasc.edu.mx>
2024-04-25 11:41:47 -0600

    Actualizacion1

--- herencia/Program.cs
+++ herencia/Program.cs
@@ -6,6 +6,16 @@ using System.Windows.Forms;
 
 namespace herencia
 {
+    /*Programa de herencia, herencia es la caracteristica de pasar atributos y  metodos a otros objetos o clases*/
+    //clase mamá
+    class Operacion
+    {
+        /*Guarda los espacios de memoria de 3 valores*/
+        //Modificador    tipo de dato       nombre
+        protected           int             valor1;
+        protected           int             valor2;
+        protected           int             Resultado;
+    }
     static class Program
     {
         /// <summary>
