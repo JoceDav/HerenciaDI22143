f9516331bc9cc0baa735d30608eed8592c81e730
JoceDav <163442742+JoceDav@users.noreply.github.com>
2024-04-25 10:53:32 -0600

    Add files via upload

---
+++ herencia/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace herencia
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.SUMAR = new System.Windows.Forms.Button();
+            this.RESTAR = new System.Windows.Forms.Button();
+            this.Multiplicar = new System.Windows.Forms.Button();
+            this.Dividir = new System.Windows.Forms.Button();
+            this.LIMPIAR = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1/ #A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor 2/#B";
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(47, 132);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(55, 13);
+            this.Resultado.TabIndex = 2;
+            this.Resultado.Text = "Resultado";
+            this.Resultado.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // SUMAR
+            // 
+            this.SUMAR.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SUMAR.Location = new System.Drawing.Point(295, 48);
+            this.SUMAR.Name = "SUMAR";
+            this.SUMAR.Size = new System.Drawing.Size(75, 29);
+            this.SUMAR.TabIndex = 3;
+            this.SUMAR.Text = "Sumar";
+            this.SUMAR.UseVisualStyleBackColor = true;
+            this.SUMAR.Click += new System.EventHandler(this.SUMAR_Click);
+            // 
+            // RESTAR
+            // 
+            this.RESTAR.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RESTAR.Location = new System.Drawing.Point(295, 83);
+            this.RESTAR.Name = "RESTAR";
+            this.RESTAR.Size = new System.Drawing.Size(75, 23);
+            this.RESTAR.TabIndex = 4;
+            this.RESTAR.Text = "Restar";
+            this.RESTAR.UseVisualStyleBackColor = true;
+            this.RESTAR.Click += new System.EventHandler(this.RESTAR_Click);
+            // 
+            // Multiplicar
+            // 
+            this.Multiplicar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Multiplicar.Location = new System.Drawing.Point(295, 112);
+            this.Multiplicar.Name = "Multiplicar";
+            this.Multiplicar.Size = new System.Drawing.Size(99, 23);
+            this.Multiplicar.TabIndex = 5;
+            this.Multiplicar.Text = "Multiplicar";
+            this.Multiplicar.UseVisualStyleBackColor = true;
+            this.Multiplicar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Dividir
+            // 
+            this.Dividir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dividir.Location = new System.Drawing.Point(295, 141);
+            this.Dividir.Name = "Dividir";
+            this.Dividir.Size = new System.Drawing.Size(75, 23);
+            this.Dividir.TabIndex = 6;
+            this.Dividir.Text = "Dividir";
+            this.Dividir.UseVisualStyleBackColor = true;
+            // 
+            // LIMPIAR
+            // 
+            this.LIMPIAR.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LIMPIAR.Location = new System.Drawing.Point(50, 160);
+            this.LIMPIAR.Name = "LIMPIAR";
+            this.LIMPIAR.Size = new System.Drawing.Size(75, 23);
+            this.LIMPIAR.TabIndex = 7;
+            this.LIMPIAR.Text = "Limpiar";
+            this.LIMPIAR.UseVisualStyleBackColor = true;
+            // 
+            // Salir
+            // 
+            this.Salir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salir.Location = new System.Drawing.Point(170, 160);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(75, 23);
+            this.Salir.TabIndex = 8;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(159, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 227);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.LIMPIAR);
+            this.Controls.Add(this.Dividir);
+            this.Controls.Add(this.Multiplicar);
+            this.Controls.Add(this.RESTAR);
+            this.Controls.Add(this.SUMAR);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Button SUMAR;
+        private System.Windows.Forms.Button RESTAR;
+        private System.Windows.Forms.Button Multiplicar;
+        private System.Windows.Forms.Button Dividir;
+        private System.Windows.Forms.Button LIMPIAR;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ herencia/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace herencia
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SUMAR_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RESTAR_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
