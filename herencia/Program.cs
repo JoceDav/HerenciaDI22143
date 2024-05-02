0b18ea64986c07d937452508983b1ef57ba398e1
JoceDav <di22143@dasc.edu.mx>
2024-05-02 10:56:05 -0600

    SI

--- herencia/Program.cs
+++ herencia/Program.cs
@@ -15,6 +15,75 @@ namespace herencia
         protected           int             valor1;
         protected           int             valor2;
         protected           int             Resultado;
+        //Atributos clase operacion
+        /*Los metos que se ponen a continuacion son las funciones de obtener el valor de la caja de texto y guardarlo en la memoria
+         * dentro de la variable indicada*/
+
+        //Usamos el constructir POR CADA CLASE DE MEMORIA
+
+        public int Valor1 //Caja de memoria 1
+        {
+            get { return valor1; } //Vamos por el valor de la caja de texto 
+            set { valor1 = value; } //Lo guarda en la memoria
+        }
+
+        public int Valor2 //Caja de memoria 2
+        {
+            get { return valor2; } //Vamos por el valor de la caja de texto 
+            set { valor2 = value; } //Lo guarda en la memoria
+        }
+
+        public int Resultado //Caja de memoria 3
+        {
+            get { return Resultado; } //Vamos por el valor de la caja de texto 
+            set { Resultado = value; } //Lo guarda en la memoria
+        }
+
+    }
+
+    //Creamos nuestras clases hijo que usara los metodos de crear dato
+    class Sumar : Operacion //Los : nos dan permiso de usar lo que tiene operacion 
+    {
+        //Las clases hijos tambien pueden tener sus propios atributos y metodos.
+        //Pueden tener variables de uso solamente para la clase hijo. 
+        //Declarar variables de esta clase 
+        //Atributos de esta clase 
+        //Metodos de clase
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1; //Accesamos al contructor de operacion 
+            valor1 = v2;
+            return Resultado = valor1 + valor2;
+        }
+
+    }
+    class Restar : Operacion //Los : nos dan permiso de usar lo que tiene operacion 
+    {
+        //Las clases hijos tambien pueden tener sus propios atributos y metodos.
+        //Pueden tener variables de uso solamente para la clase hijo. 
+        //Declarar variables de esta clase 
+        //Atributos de esta clase 
+        //Metodos de clase
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1; //Accesamos al contructor de operacion 
+            valor1 = v2;
+            return Resultado = valor1 - valor2;
+        }
+    }
+    class Multiplicar : Operacion //Los : nos dan permiso de usar lo que tiene operacion 
+    {
+        //Las clases hijos tambien pueden tener sus propios atributos y metodos.
+        //Pueden tener variables de uso solamente para la clase hijo. 
+        //Declarar variables de esta clase 
+        //Atributos de esta clase 
+        //Metodos de clase
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1; //Accesamos al contructor de operacion 
+            valor1 = v2;
+            return Resultado = valor1 * valor2;
+        }
     }
     static class Program
     {
